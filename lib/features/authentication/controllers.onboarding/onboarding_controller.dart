c2fa366c9264b6dcd4dde132744506dae39b8409
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-05-05 18:01:23 +0300

    add on-boarding screens for the app

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../screens/login/login.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
+
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      Get.offAll(const LoginScreen());
+    } else {
+      currentPageIndex.value++;
+      pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
+    }
+  }
+
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+}
---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -0,0 +1,34 @@
+import 'package:ecom_sel/features/authentication/controllers.onboarding/onboarding_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../../../../../utlis/constants/colors.dart';
+import '../../../../../utlis/device/device_utility.dart';
+import '../../../../../utlis/helpers/helper_functions.dart';
+
+class OnBoardingDotNavigation extends StatelessWidget {
+  const OnBoardingDotNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = OnBoardingController.instance;
+    final dark = EHelperFunctions.isDarkMode(context);
+    return Positioned(
+      bottom: EDeviceUtils.getBottomNavigationBarHeight() * 7,
+      right: 0,
+      left: 0,
+      child: Center(
+        child: SmoothPageIndicator(
+          controller: controller.pageController,
+          onDotClicked: controller.dotNavigationClick,
+          count: 3,
+          effect: ExpandingDotsEffect(
+            activeDotColor: dark ? EColors.light : EColors.dark,
+          ),
+        ),
+      ),
+    );
+  }
+}

a54292026d6c9cef531d42ce4ac5dee5de1dbd68
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-05-11 17:42:00 +0300

    organize project structure by moving App to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
-
-import 'package:ecom_sel/utlis/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+import 'app.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: EAppTheme.lightTheme,
-      darkTheme: EAppTheme.darkTheme,
-    );
-  }
+void main(){
+  runApp(App());
 }
+
+

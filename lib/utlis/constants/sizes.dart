9e148c50357424e6718c27803125bf3342bc8d2e
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-05-31 12:50:01 +0300

    update the size between sections for a better design feel

--- lib/utlis/constants/sizes.dart
+++ lib/utlis/constants/sizes.dart
@@ -33,7 +33,7 @@ class ESizes {
   // Default spacing between sections
   static const double defaultSpace = 24.0;
   static const double spaceBtwItems = 16.0;
-  static const double spaceBtwSections = 32.0;
+  static const double spaceBtwSections = 20.0;
 
   // Border radius
   static const double borderRadiusSm = 4.0;

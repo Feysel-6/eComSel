0feec772231244ca8bc4024dc8d90c4ee501cae2
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-06-07 22:03:46 +0300

    Refactor: streamline cart screen imports

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,13 +1,10 @@
 import 'package:ecom_sel/common/widgets/appbar/appbar.dart';
-import 'package:ecom_sel/common/widgets/texts/product_price_text.dart';
 import 'package:ecom_sel/features/shop/screens/cart/widgets/cart_items.dart';
 import 'package:ecom_sel/features/shop/screens/checkout/checkout.dart';
 import 'package:ecom_sel/utlis/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../../common/widgets/products/cart/add_remove_button.dart';
-import '../../../../common/widgets/products/cart/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});

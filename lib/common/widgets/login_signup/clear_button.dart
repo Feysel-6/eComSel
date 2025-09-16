b21a707ae69b7369c8e9d6f32dbdcaead0b585c6
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-09-16 03:47:24 +0300

    Refactor: Use AuthenticationRepository for logout in ClearButton

--- lib/common/widgets/login_signup/clear_button.dart
+++ lib/common/widgets/login_signup/clear_button.dart
@@ -1,8 +1,7 @@
+import 'package:ecom_sel/data/repositories/authentication/authentication_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import '../../../features/authentication/screens/login/login.dart';
 
 class ClearButton extends StatelessWidget {
   const ClearButton({
@@ -12,7 +11,7 @@ class ClearButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => Get.offAll(() => const LoginScreen()),
+      onPressed: () => AuthenticationRepository.instance.logout(),
       icon: const Icon(CupertinoIcons.clear),
     );
   }

55b74b66a351eb482ea5be69e4e7c198c9f2349a
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2025-10-15 11:18:39 +0300

    feat: Handle unverified email on login
    This commit enhances the login process by redirecting users with unconfirmed emails to the verification screen and automatically resending the verification email. It also corrects a typo in a loading message.

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:ecom_sel/data/repositories/authentication/authentication_repository.dart';
+import 'package:ecom_sel/features/authentication/controllers/signup/verify_email_controller.dart';
 import 'package:ecom_sel/features/personalization/controllers/user_controller.dart';
 import 'package:ecom_sel/utlis/popups/full_screen_loader.dart';
 import 'package:flutter/material.dart';
@@ -6,8 +7,10 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 import '../../../../utlis/constants/image_strings.dart';
+import '../../../../utlis/exceptions/supabase_auth_exceptions.dart';
 import '../../../../utlis/helpers/network_manager.dart';
 import '../../../../utlis/loaders/loaders.dart';
+import '../../screens/signup/verify_email.dart';
 
 class LoginController extends GetxController {
   final rememberMe = false.obs;
@@ -18,6 +21,7 @@ class LoginController extends GetxController {
   GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
 
   final userController = Get.put(UserController());
+  final verifyEmailController = Get.put(VerifyEmailController());
 
   // @override
   // void onInit() {
@@ -28,7 +32,7 @@ class LoginController extends GetxController {
   Future<void> emailAndPasswordSignIn() async {
     try {
       EFullScreenLoader.openLoadingDialog(
-        'Loggin you in...',
+        'Logging you in...',
         EImages.docerAnimation,
       );
 
@@ -44,10 +48,10 @@ class LoginController extends GetxController {
 
       if (!loginFormKey.currentState!.validate()) return;
 
-      if (rememberMe.value) {
-        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
-        localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
-      }
+      // if (rememberMe.value) {
+      //   localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+      //   localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+      // }
 
       await AuthenticationRepository.instance.loginWithEmailAndPassword(
         email.text.trim(),
@@ -62,7 +66,15 @@ class LoginController extends GetxController {
       );
     } catch (e) {
       EFullScreenLoader.stopLoading();
-      ELoaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+      if(e is EAuthException && e.message.contains('Email not confirmed')){
+        Get.to(() => VerifyEmailScreen(
+          email: email.text.trim(),
+          password: password.text.trim(),
+        ));
+        await verifyEmailController.resendVerificationEmail(email.text.trim());
+      } else {
+        ELoaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+      }
     }
   }
 
